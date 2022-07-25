2d4aeec8085f44b47e24fb77ba14af4d513dddcc
Kareem <kxreem@gmail.com>
2022-07-25 10:37:13 -0400

    Preloader

---
+++ client/src/components/Preloader.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Preloader = () => (
+  <Wrapper>
+    <Spinner />
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const Spinner = styled.div`
+  display: block;
+  margin: auto;
+  width: 50px;
+  height: 50px;
+  border: 8px solid rgba(0, 0, 0, 0.1);
+  border-bottom-color: white;
+  border-radius: 50%;
+  animation: spin 0.75s infinite linear;
+`;
+
+export default Preloader;
