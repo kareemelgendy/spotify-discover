8285da5267321d665d7ec6d8a61bc9c8521564ab
Kareem <kxreem@gmail.com>
2022-07-25 08:43:21 -0400

    Added server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const axios = require("axios")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+app.use(cors())
+
+// Authorization Code Flow Documentation
+// https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
+
+// Spotify API Endpoints
+// https://developer.spotify.com/documentation/web-api/reference/
+
+app.get("/", (_, res) => {
+  res.send("Spotify Wrapped")
+})
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server live.")
+})
